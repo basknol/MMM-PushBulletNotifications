898d4b172b094731931095b893185bf9168d3f52
oemel09 <github@oemel09.de>
2019-12-23 12:15:48 +0100

    Adds end-to-end-encryption

--- node_helper.js
+++ node_helper.js
@@ -38,7 +38,7 @@ module.exports = NodeHelper.create({
             this.info(notification + " received");
 
             //Check if old config values are used, log warning
-            if (!this.connected) {                
+            if (!this.connected) {
                 if (this.config.showNotificationsOnLoad !== undefined) {
                     this.warning("[Obsolete] The configuration option 'showNotificationsOnLoad' is no longer used and renamed to 'showPushesOnLoad'");
                     this.config.showPushesOnLoad = this.config.showNotificationsOnLoad;
@@ -58,93 +58,98 @@ module.exports = NodeHelper.create({
             //Get devices first
             this.getDevicesAsync(this.config, this.pusher).then(function (response) {
                 //Load pushes on start if we need to
-                if (self.config.showPushesOnLoad) {                
-                    self.loadPushes(self.pusher, self.config, true);                
+                if (self.config.showPushesOnLoad) {
+                    self.loadPushes(self.pusher, self.config, true);
                 }
-            });                
+            });
 		}
 	},
 
     //Start listening to pushbullet
 	pushBulletListener: function(config){
 		this.pusher = new PushBullet(config.accessToken); // PushBulletAPI object
-		var stream = this.pusher.stream();
-		var self = this;
+        const self = this;
+        this.pusher.me(function(error, user) {
+            // needed to call me() to gather user iden
+            self.pusher.enableEncryption(config.endToEndPassword, user.iden);
 
-        //Connect
-		stream.on('connect', function() {
-			// stream has connected
-            self.info("PushBullet connected");
-            self.connected = true;
-		});
+            const stream = self.pusher.stream();
 
-        //Logs errors
-        stream.on('error', function (error) {
-            self.error("Push stream error: "+error);                
-        });
-
-        //Watch for normal pushes if configured
-        if (config.showPushes) {
-            /*PushBullet API
-             * https://docs.pushbullet.com/#realtime-event-stream
-             * 
-             * Watch for normal pushes not Ephemerals. Tickle means something has changed on server, subtype tells what has changed.
-             */
-            stream.on("tickle", function (type) {
-                self.debug("tickle received: " + type);
-                if (type === "push") {
-                    self.loadPushes(self.pusher, config, false);
-                }
+            //Connect
+            stream.on('connect', function() {
+                // stream has connected
+                self.info("PushBullet connected");
+                self.connected = true;
             });
-        }
 
-        //Watch for mirrored notifications or SMS (ephemerals)
-        if (config.showMirroredNotifications || config.showSMS) {
-            /*
-             * Ephemerals
-             */
-            stream.on("push", function (push) {
-                self.json(push, "Ephemeral received");
-                if (push.type === "mirror" && config.showMirroredNotifications) {
-                    self.debug("Mirrored Notification received, sending to mirror");
+            //Logs errors
+            stream.on('error', function (error) {
+                self.error("Push stream error: "+error);
+            });
 
-                    self.playSound(config);
+            //Watch for normal pushes if configured
+            if (config.showPushes) {
+                /*PushBullet API
+                 * https://docs.pushbullet.com/#realtime-event-stream
+                 *
+                 * Watch for normal pushes not Ephemerals. Tickle means something has changed on server, subtype tells what has changed.
+                 */
+                stream.on("tickle", function (type) {
+                    self.debug("tickle received: " + type);
+                    if (type === "push") {
+                        self.loadPushes(self.pusher, config, false);
+                    }
+                });
+            }
 
-                    //Sending mirrored notification to mirror
-                    self.sendSocketNotification("MIRROR", push);
-                }
-                else if (push.type === "sms_changed" && config.showSMS) {
-                    self.debug("SMS received, sending to mirror");
+            //Watch for mirrored notifications or SMS (ephemerals)
+            if (config.showMirroredNotifications || config.showSMS) {
+                /*
+                 * Ephemerals
+                 */
+                stream.on("push", function (push) {
+                    self.json(push, "Ephemeral received");
+                    if (push.type === "mirror" && config.showMirroredNotifications) {
+                        self.debug("Mirrored Notification received, sending to mirror");
 
-                    //Do we have the info to display the SMS inside notifications, otherwise ignore it. Sometimes empty, not sure why
-                    if (push.notifications.length > 0) {
                         self.playSound(config);
 
-                        //Sending SMS to mirror
-                        self.sendSocketNotification("SMS", push);
+                        //Sending mirrored notification to mirror
+                        self.sendSocketNotification("MIRROR", push);
                     }
-                }
-                else if (push.type === "dismissal") {
-                    self.debug("Dismissal received, sending to mirror");
+                    else if (push.type === "sms_changed" && config.showSMS) {
+                        self.debug("SMS received, sending to mirror");
 
-                    //Sending dismissal to mirror
-                    self.sendSocketNotification("DISMISSAL", push);
-                }
-            });
-        }
+                        //Do we have the info to display the SMS inside notifications, otherwise ignore it. Sometimes empty, not sure why
+                        if (push.notifications.length > 0) {
+                            self.playSound(config);
 
-        //Show a warning if the config is probably not setup correctly
-        if (!config.showMirroredNotifications && !config.showSMS && !config.showPushes) {
-            self.warning("Please check the MMM-PushBulletNotifications module configuration. At least one of these properties must be set to true to be able to show notifications: showPushes, showMirroredNotifications or showSMS");
-        }
+                            //Sending SMS to mirror
+                            self.sendSocketNotification("SMS", push);
+                        }
+                    }
+                    else if (push.type === "dismissal") {
+                        self.debug("Dismissal received, sending to mirror");
 
-        //Connect the stream
-        stream.connect();
+                        //Sending dismissal to mirror
+                        self.sendSocketNotification("DISMISSAL", push);
+                    }
+                });
+            }
+
+            //Show a warning if the config is probably not setup correctly
+            if (!config.showMirroredNotifications && !config.showSMS && !config.showPushes) {
+                self.warning("Please check the MMM-PushBulletNotifications module configuration. At least one of these properties must be set to true to be able to show notifications: showPushes, showMirroredNotifications or showSMS");
+            }
+
+            //Connect the stream
+            stream.connect();
+        });
     },
 
     loadPushes: function (pusher, config, init) {
-        var self = this;                
-        
+        var self = this;
+
         //Get pushes and filter out 'command pushes' and check if a target device filter is configured
         self.getFilteredPushesAsync(pusher, config, init).then(function (result) {
             if (result != null) {
@@ -157,7 +162,7 @@ module.exports = NodeHelper.create({
                 self.sendSocketNotification("PUSHES", result);
             }
         });
-        
+
     },
 
     //Play sound
@@ -176,7 +181,7 @@ module.exports = NodeHelper.create({
     //Get devices async
     getDevicesAsync: async function (config, pusher) {
         var self = this;
-                
+
         //Only get devices if we did not already
         if (this.devices.length == 0) {
             var deviceOptions = {
@@ -198,18 +203,18 @@ module.exports = NodeHelper.create({
                 }
             });
         }
-        else {            
+        else {
             //Send devices to mirror
             self.sendSocketNotification("DEVICES", this.devices);
         }
 
         return this.devices;
 	},
-    
+
     //Get and filter pushes async
 	getFilteredPushesAsync: async function(pusher, config, init) {
         var self = this;
-        var pushes = [];		
+        var pushes = [];
         var cursor = null;
 
 		//Max 3 fetch rounds
@@ -218,7 +223,7 @@ module.exports = NodeHelper.create({
             self.json(response, "Responses");
 
             if (response != null && response.pushes != null && response.pushes.length > 0) {
-                var responsePushes = response.pushes;                
+                var responsePushes = response.pushes;
 
                 //Command Magic Mirror (execute if push starts with mm:)(do not execute on initializing, starting mirror)
                 if (!init && i == 0 && responsePushes[0].body != null && responsePushes[0].body.startsWith("mm:")) {
@@ -226,12 +231,12 @@ module.exports = NodeHelper.create({
                     break;
                 }
 
-				//Filter pushes if we need to                
-                responsePushes = this.filterPushes(config, response.pushes, self.devices);				
+				//Filter pushes if we need to
+                responsePushes = this.filterPushes(config, response.pushes, self.devices);
 
 				//Add pushes to return array
-				responsePushes.forEach(function(p) {                                        
-                    pushes.push(p);                    
+				responsePushes.forEach(function(p) {
+                    pushes.push(p);
 				});
 
 				//Are there more pushes (cursor?) and do we need to fetch more pushes?
@@ -276,7 +281,7 @@ module.exports = NodeHelper.create({
             if (error) {
                 self.error("Error fetching pushes. " + error);
             }
-                        
+
             result = response;
         });
 
@@ -418,9 +423,9 @@ module.exports = NodeHelper.create({
         if (message) {
             this.log("debug", message);
         }
-        this.log("json", json);        
+        this.log("json", json);
     },
-    debug: function (message) {        
+    debug: function (message) {
         this.log("debug", message);
     },
     info: function (message) {
@@ -432,12 +437,12 @@ module.exports = NodeHelper.create({
     error: function (message) {
         this.log("error", message);
     },
-    log: function (type, message) {                
+    log: function (type, message) {
         var currentTime = new Date();
         var now = currentTime.toLocaleTimeString('en-US', { hour12: false }) + "." + currentTime.getMilliseconds();
         switch (type.toLowerCase()) {
             case "json":
-                if (this.debugMode) {                    
+                if (this.debugMode) {
                     console.log(message);
                 }
                 break;
@@ -448,18 +453,18 @@ module.exports = NodeHelper.create({
                 break;
 
             default:
-            case "info":                
+            case "info":
                 console.log("["+this.name+"][Info] " + now + " - " + message);
                 break;
 
-            case "warning":                
+            case "warning":
                 console.log("["+this.name+"][Warning] " + now + " - " + message);
                 break;
 
-            case "error":                
+            case "error":
                 console.log("["+this.name+"][Error] " + now + " - " + message);
-                break;            
-            
-        }                
+                break;
+
+        }
     }
 });
